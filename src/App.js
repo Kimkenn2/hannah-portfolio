dc4af9ef834f46795a96132526371ad97065b01c
Kenneth Kim <kkm2059@gmail.com>
2022-06-07 13:46:18 -0400

    Added ReactRouterDom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BrowseArt from './Components/BrowseArt';
+import HomePage from './Components/HomePage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
+      </BrowserRouter>
       
     </div>
   );
