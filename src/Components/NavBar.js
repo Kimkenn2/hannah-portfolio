2c9ddcd13f36b1236b2532a02d165d0321f5efed
Kenneth Kim <kkm2059@gmail.com>
2022-06-10 12:15:02 -0400

    Created NavBar Template

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <div className="mainNavDiv">
+           
+            <ul className="navBarUl">
+                {/* <Link to="/browseart" className="navBarLink">BROWSE ART</Link> */}
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
