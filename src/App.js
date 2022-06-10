9d58b4ab03faff2e049cc903eccb0d654c43a070
Kenneth Kim <kkm2059@gmail.com>
2022-06-09 22:15:12 -0400

    Created ArtCard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BrowseArt from './Components/BrowseArt';
 import HomePage from './Components/HomePage';
 import PortfolioPage from "./Components/PortfolioPage";
+import ArtCard from "./Components/ArtCard";
 import './App.css';
+
+
 
 function App() {
   return (
---
+++ src/Components/ArtCard.js
@@ -0,0 +1,10 @@
+function ArtCard() {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ArtCard
