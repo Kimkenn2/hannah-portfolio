1aef27ea17c0034ad8a071985b494c34c1d9bece
Kenneth Kim <kkm2059@gmail.com>
2022-06-11 20:29:21 -0400

    Created BrowseArt Route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}/>
+        <Route path="/browseArt" element={<BrowseArt />} />
       </Routes>
       </BrowserRouter>
       
